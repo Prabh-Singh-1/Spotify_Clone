6c339c7f3a8b01bde960cfbf2f26605240418f8d
Prabh-Singh-1 <workprabhsingh1.com>
2024-07-13 11:46:54 +0530

    final commit

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     console.log(response);
     let div = document.createElement("div");    
