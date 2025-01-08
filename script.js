d5afbeec326aa3d0a80f064e9263b32aa7b83998
Prabh-Singh-1 <workprabhsingh1.com>
2025-01-08 18:28:08 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ const playMusic = async (track, pause = false) => {
 };
 
 async function main() {
-    await getSongs("https://spotify-clone-zeta-beige.vercel.app/Spotify_Clone/song/ncs/");
+    await getSongs("song/ncs/");
     console.log('Songs: ', getSongs);
     // Wait for user interaction before playing the first song
     document.addEventListener("click", () => {
