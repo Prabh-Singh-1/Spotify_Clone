aea03f90bcd2cece97a55a62f53a8294f09a2e7e
Prabh-Singh-1 <workprabhsingh1.com>
2025-01-08 19:14:05 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    const proxyUrl = "https://prabh-singh-1.github.io/Spotify_Clone/"; 
+    const proxyUrl = "https://prabh-singh-1.github.io/Spotify_Clone"; 
     let a = await fetch(`${proxyUrl}/song/ncs/`);
     console.log('path ', a);
     let response = await a.text();
